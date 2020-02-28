f0c19ce0af04b195f980a614a56acadbf341e3d3
Ryan Hopper-Lowe <ryan.hopper-lowe@sjsu.edu>
2020-02-27 21:05:13 -0800

    Added baseui to header

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import * as serviceWorker from './serviceWorker';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider } from 'baseui';
+const engine = new Styletron();
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+const Root = () => (
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <AppRouter />
+    </BaseProvider>
+  </StyletronProvider>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import DashboardPage from '../pages/DashboardPage/DashboardPage';
-import Header from '../components/Header';
+import Header from '../components/HeaderNav';
 import VotePage from '../pages/VotePage/VotePage';
 
 const AppRouter = () => (
